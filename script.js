337732c2d90c7d3ccf848d705515b40f92eee553
devin <devinpixels25@gmail.com>
2024-06-02 01:25:58 -0400

    added getHumanChoice function

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function getComputerChoice () {
         return 'scissors'
     }
 }
+
+function getHumanChoice (choice) {
+    choice = prompt('rock,paper, or scissors?').toLowerCase()
+    if (choice == 'rock') {
+        return choice;
+    } else if (choice == 'paper') {
+        return choice;
+    } else if (choice == 'scissors') {
+        return choice;
+    } else {
+        alert('abort')
+    }
+}
