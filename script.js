98debc53b53034696d20d2153e9d41d522cb35e8
devin <devinpixels25@gmail.com>
2024-06-01 23:55:47 -0400

    add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('this works')
+// console.log('this works')
+
+function getComputerChoice () {
+    let randomNum = Math.floor(Math.random() * 3)
+   // console.log(randomNum)
+    if (randomNum === 0) {
+        return 'rock'}
+    else if (randomNum === 1) {
+        return 'paper' }
+    else if (randomNum === 2) {
+        return 'scissors'
+    }
+}
