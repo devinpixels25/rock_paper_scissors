71d66b28615949b357ea34ef578118b5c7709b43
devin <devinpixels25@gmail.com>
2024-06-04 00:12:33 -0400

    added function playRound

--- script.js
+++ script.js
@@ -24,3 +24,63 @@ function getHumanChoice (choice) {
         alert('abort')
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    
+    if (humanChoice === 'rock') {
+        if (computerChoice === 'rock') {
+            console.log('TIE');
+        }
+
+        if (computerChoice === 'paper') {
+            computerScore += 1;
+            console.log('computer wins this round');
+        }
+
+        if (computerChoice === 'scissors') {
+            humanScore += 1;
+            console.log('player wins this round')
+        }
+    }
+
+
+    if (humanChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            humanChoice += 1;
+            console.log('player wins this round');
+        }
+
+        if (computerChoice === 'paper') {
+            console.log('TIE');
+        }
+
+        if (computerChoice === 'scissors') {
+            computerScore += 1;
+            console.log('computer wins this round.');
+        }
+    }
+
+
+    if (humanChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            computerScore += 1;
+            console.log('computer wins this round')
+        }
+
+        if (computerChoice === 'paper') {
+            humanScore += 1;
+            console.log('player wins this round')
+        }
+
+        if (computerChoice === 'scissors') {
+            console.log('TIE');
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
